a9348db86aaa2272099372ab1f17493b66aa0756
salman24823 <salman248shah@gmail.com>
2025-05-30 23:21:17 +0500

    AASD

--- src/app/Components/Footer/page.jsx
+++ src/app/Components/Footer/page.jsx
@@ -65,9 +65,9 @@ const Footer = () => {
           <span className="point">Social Links</span>
 
           <div className="flex flex-col gap-2">
-            <Link href={"https://www.linkedin.com/in/arafa-webs-74a8b6367/"}>LinkedIn</Link>
-            <Link href={"/"}>Facebook</Link>
-            <Link href={"/"}>Instagram</Link>
+            <a target="_blank" href={"https://www.linkedin.com/in/arafa-webs-74a8b6367/"}>LinkedIn</a>
+            <a target="_blank" href={"#"}>Facebook</a>
+            <a target="_blank" href={"#"}>Instagram</a>
           </div>
         </div>
         <div className="lg:col-span-1 flex flex-col gap-4">
@@ -89,10 +89,10 @@ const Footer = () => {
             <span className="text-gray-400">All right reserved © 2025 Arafawebs</span>
           </div>
           <div className="flex gap-10 lg:gap-5 items-center  justify-center">
-            <a href="https://www.linkedin.com/in/arafa-webs-74a8b6367/"><FaLinkedin className="text-2xl" /></a>
-            <a href="https://github.com/arafamaster001"><FaGithub className="text-2xl" /></a>
-            <span><RiTwitterXLine className="text-2xl" /></span>
-            <span><FaDiscord className="text-3xl" /></span>
+            <a target="_blank" href="https://www.linkedin.com/in/arafa-webs-74a8b6367/"><FaLinkedin className="text-2xl" /></a>
+            <a target="_blank" href="https://github.com/arafamaster001"><FaGithub className="text-2xl" /></a>
+            <a target="_blank" href="#"><RiTwitterXLine className="text-2xl" /></a>
+            <a target="_blank" href="#"><FaDiscord className="text-3xl" /></a>
 
           </div>
         </div>
